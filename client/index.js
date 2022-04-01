cae45f85f57155fa307d2e9b64f0dc1004b9fdb4
brianorora <brianorora@gmail.com>
2022-04-02 00:35:29 +0300

    Add client/issues.js

--- client/index.js
+++ client/index.js
@@ -10,14 +10,14 @@ const yargs = require("yargs");
 const options = yargs
  .usage("Usage: -n <name>")
   .command('create', 'create an issue', {
-    title: {type: '', demmandOption: true},
+    title: {type: '', demandOption: true},
     description: {type: '', demandOption: true}
   }, createIssue)
-  .command('read', 'read an issue', { id: { type: 'number', demmandOption: true}}, getIssue)
+  .command('read', 'read an issue', { id: { type: 'number', demandOption: true}}, getIssue)
   .command('update', 'update an issue', {
-      id: { demmandOption: true, type:  'number'},
+      id: { demandOption: true, type:  'number'},
       title: { type: 'string'},
       description: {type: 'string'}
     }, updateIssue)
-  .command('delete', 'delete an issue', { id: {type: 'number', demmandOption: true } }, deleteIssue)
+  .command('delete', 'delete an issue', { id: {type: 'number', demandOption: true } }, deleteIssue)
  .argv;
---
+++ client/issues.js
@@ -0,0 +1,54 @@
+const fetch = require('node-fetch');
+
+const port = 8080;
+const baseUrl = `http://localhost:${port}/issues`
+
+const headers = {'content-type': 'application/json'};
+
+function getIssue(args) {
+  const { id } = args;
+  const issue = fetch(`${baseUrl}/${id}`)
+    .then(resp => resp.json())
+  console.log('created new issue', issue)
+}
+
+function updateIssue(args) {
+  const { id, description, title } = args;
+  const body = JSON.stringify({
+    id,
+    description,
+    title
+  })
+  return fetch(baseUrl, {
+    method: 'PATCH',
+    headers,
+    body
+  }).then(() => console.log(`updated issue #${id}`))
+}
+
+function deleteIssue(args) {
+  const { id } = args;
+  const body = JSON.stringify({id});
+  fetch(baseUrl, {
+    method: 'DELETE',
+    headers,
+    body
+  }).then(() => console.log(`deleted issue #${id}`))
+}
+
+function createIssue(args) {
+  const { title, description } = args;
+  const body = JSON.stringify({title, description})
+  return fetch(baseUrl, {
+    method: 'POST',
+    headers,
+    body
+  });
+}
+
+module.exports = {
+  getIssue,
+  updateIssue,
+  deleteIssue,
+  createIssue,
+}
